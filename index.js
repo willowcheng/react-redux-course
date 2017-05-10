3741185122b8ff364c82118197fa2239beae128a
willowcheng <e.wingus@gmail.com>
2017-05-10 14:36:45 -0400

    Create redux store

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import PlayerReducer from './src/reducers/player'
 import Scoreboard from './src/containers/Scoreboard.js'
 
+const store = createStore(
+  PlayerReducer
+)
+
 render(
-  <Scoreboard/>,
+  <Provider store={store}>
+    <Scoreboard/>
+  </Provider>,
   document.getElementById('root')
 )
